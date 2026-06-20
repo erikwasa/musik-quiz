60ae9f5d74d6520552431d488c2bf1846052b4d2
Erik Wahlström <54457926+erikwasa@users.noreply.github.com>
2026-06-20 21:06:37 +0200

    Create svensk-sommar.js

---
+++ data/svensk-sommar.js
@@ -0,0 +1,31 @@
+    "title": "Sunnanvind",
+    "spotifyUrl": "https://open.spotify.com/track/6FeOQ2oiV6z6xIoFSI9Y1T"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2019,
+    "artist": "Moonica Mac",
+    "title": "Till slutet av augusti",
+    "spotifyUrl": "https://open.spotify.com/track/2m2errv0ukOnsFL2mAOifj"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2020,
+    "artist": "Victor Leksell",
+    "title": "Svag",
+    "spotifyUrl": "https://open.spotify.com/track/4SS4YfDAEQG0fZAe1xfgn3"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2021,
+    "artist": "Benjamin Ingrosso",
+    "title": "Allt det vackra",
+    "spotifyUrl": "https://open.spotify.com/track/3Zta1ilm1Qte2bSkTAgAqI"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2023,
+    "artist": "Miss Li",
+    "title": "Hälsa Gud",
+    "spotifyUrl": "https://open.spotify.com/track/5g9gZGiXlk747pESQzmmRT"
+  }
