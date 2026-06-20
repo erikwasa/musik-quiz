75b48354adfa0c5fe80a16cc28a5277ee2087038
Erik Wahlström <54457926+erikwasa@users.noreply.github.com>
2026-06-20 16:45:30 +0200

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,153 @@
+/*
+  Song data for Guess the Song Year.
+
+  Edit this file when you want to add, remove, or change songs.
+
+  Notes:
+  - category controls which category button the song appears under.
+  - year is the quiz answer shown after pressing "Reveal answer".
+  - spotifyUrl should be a Spotify track URL.
+*/
+
+window.songData = [
+  {
+    category: "70s & earlier",
+    year: 1975,
+    artist: "Queen",
+    title: "Bohemian Rhapsody",
+    spotifyUrl: "https://open.spotify.com/track/4u7EnebtmKWzUH433cf5Qv"
+  },
+  {
+    category: "70s & earlier",
+    year: 1976,
+    artist: "ABBA",
+    title: "Dancing Queen",
+    spotifyUrl: "https://open.spotify.com/track/0GjEhVFGZW8afUYGChu3Rr"
+  },
+  {
+    category: "80s",
+    year: 1982,
+    artist: "Toto",
+    title: "Africa",
+    spotifyUrl: "https://open.spotify.com/track/2374M0fQpWi3dLnB54qaLX"
+  },
+  {
+    category: "80s",
+    year: 1983,
+    artist: "Michael Jackson",
+    title: "Billie Jean",
+    spotifyUrl: "https://open.spotify.com/track/5ChkMS8OtdzJeqyybCc9R5"
+  },
+  {
+    category: "80s",
+    year: 1985,
+    artist: "a-ha",
+    title: "Take on Me",
+    spotifyUrl: "https://open.spotify.com/track/2WfaOiMkCvy7F5fcp2zZ8L"
+  },
+  {
+    category: "80s",
+    year: 1987,
+    artist: "Whitney Houston",
+    title: "I Wanna Dance with Somebody (Who Loves Me)",
+    spotifyUrl: "https://open.spotify.com/track/2tUBqZG2AbRi7Q0BIrVrEj"
+  },
+  {
+    category: "90s",
+    year: 1991,
+    artist: "Nirvana",
+    title: "Smells Like Teen Spirit",
+    spotifyUrl: "https://open.spotify.com/track/5ghIJDpPoe3CfHMGu71E6T"
+  },
+  {
+    category: "90s",
+    year: 1998,
+    artist: "Britney Spears",
+    title: "...Baby One More Time",
+    spotifyUrl: "https://open.spotify.com/track/3MjUtNVVq3C8Fn0MP3zhXa"
+  },
+  {
+    category: "90s",
+    year: 1999,
+    artist: "Backstreet Boys",
+    title: "I Want It That Way",
+    spotifyUrl: "https://open.spotify.com/track/6e40mgJiCid5HRAGrbpGA6"
+  },
+  {
+    category: "00s",
+    year: 2003,
+    artist: "Outkast",
+    title: "Hey Ya!",
+    spotifyUrl: "https://open.spotify.com/track/2PpruBYCo4H7WOBJ7Q2EwM"
+  },
+  {
+    category: "00s",
+    year: 2003,
+    artist: "Beyoncé",
+    title: "Crazy In Love (feat. JAY-Z)",
+    spotifyUrl: "https://open.spotify.com/track/5IVuqXILoxVWvWEPm82Jxr"
+  },
+  {
+    category: "00s",
+    year: 2003,
+    artist: "The Killers",
+    title: "Mr. Brightside",
+    spotifyUrl: "https://open.spotify.com/track/003vvx7Niy0yvhvHt4a68B"
+  },
+  {
+    category: "00s",
+    year: 2008,
+    artist: "Lady Gaga",
+    title: "Poker Face",
+    spotifyUrl: "https://open.spotify.com/track/5R8dQOPq8haW94K7mgERlO"
+  },
+  {
+    category: "10s",
+    year: 2010,
+    artist: "Robyn",
+    title: "Dancing On My Own",
+    spotifyUrl: "https://open.spotify.com/track/7g13jf3zqlP5S68Voo5v9m"
+  },
+  {
+    category: "10s",
+    year: 2010,
+    artist: "Adele",
+    title: "Rolling in the Deep",
+    spotifyUrl: "https://open.spotify.com/track/1CkvWZme3pRgbzaxZnTl5X"
+  },
+  {
+    category: "10s",
+    year: 2013,
+    artist: "Daft Punk",
+    title: "Get Lucky",
+    spotifyUrl: "https://open.spotify.com/track/2Foc5Q5nqNiosCNqttzHof"
+  },
+  {
+    category: "10s",
+    year: 2013,
+    artist: "Avicii",
+    title: "Wake Me Up",
+    spotifyUrl: "https://open.spotify.com/track/4h8VwCb1MTGoLKueQ1WgbD"
+  },
+  {
+    category: "10s",
+    year: 2014,
+    artist: "Taylor Swift",
+    title: "Shake It Off",
+    spotifyUrl: "https://open.spotify.com/track/5xTtaWoae3wi06K5WfVUUH"
+  },
+  {
+    category: "10s",
+    year: 2019,
+    artist: "The Weeknd",
+    title: "Blinding Lights",
+    spotifyUrl: "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b"
+  },
+  {
+    category: "20s",
+    year: 2021,
+    artist: "Olivia Rodrigo",
+    title: "drivers license",
+    spotifyUrl: "https://open.spotify.com/track/5wANPM4fQCJwkGd4rN57mH"
+  }
+];
