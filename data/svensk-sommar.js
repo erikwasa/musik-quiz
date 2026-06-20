22957d3cd1204f69147142005745ba0244f1202f
Erik Wahlström <54457926+erikwasa@users.noreply.github.com>
2026-06-20 21:16:57 +0200

    Update svensk-sommar.js

--- data/svensk-sommar.js
+++ data/svensk-sommar.js
@@ -1,3 +1,218 @@
+ window.quizCategories["Svensk sommar"] = [
+  {
+    "category": "Svensk sommar",
+    "year": 1941,
+    "artist": "Evert Taube",
+    "title": "Sjösala vals",
+    "spotifyUrl": "https://open.spotify.com/track/5rTZToEwovN6JEBcPps7Ph"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1964,
+    "artist": "Olle Adolphson",
+    "title": "Det gåtfulla folket",
+    "spotifyUrl": "https://open.spotify.com/track/4LZ6iqSwM5YhJevsKv4wFn"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1966,
+    "artist": "Mats Paulson",
+    "title": "Visa vid vindens ängar",
+    "spotifyUrl": "https://open.spotify.com/track/2f9Wq2T8qty2EmsUMNNPxq"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1971,
+    "artist": "Sven-Bertil Taube",
+    "title": "Änglamark",
+    "spotifyUrl": "https://open.spotify.com/track/7fe7diX68FCiicF46Nfial"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1973,
+    "artist": "Ted Gärdestad",
+    "title": "Sol, vind och vatten",
+    "spotifyUrl": "https://open.spotify.com/track/0lwJH1BDr8Z4wfXvi6yC9y"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1973,
+    "artist": "Georg Riedel, Lisa Nilsson",
+    "title": "Idas sommarvisa",
+    "spotifyUrl": "https://open.spotify.com/track/41ykywmUtDSis7fGAarryh"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1978,
+    "artist": "Cornelis Vreeswijk",
+    "title": "Turistens klagan",
+    "spotifyUrl": "https://open.spotify.com/track/5dXdy6YR6lxxC3GxBZcRWv"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1980,
+    "artist": "Snowstorm",
+    "title": "Sommarnatt",
+    "spotifyUrl": "https://open.spotify.com/track/2gx7R4S2ElylsA44lD0ubk"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1982,
+    "artist": "Gyllene Tider",
+    "title": "Sommartider",
+    "spotifyUrl": "https://open.spotify.com/track/1d1BeKPNgaQ36wcxGqnfU3"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1982,
+    "artist": "Ulf Lundell",
+    "title": "Öppna landskap",
+    "spotifyUrl": "https://open.spotify.com/track/1rudppuYTKpVCtWoocDiS1"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1982,
+    "artist": "Tomas Ledin",
+    "title": "Sommaren är kort",
+    "spotifyUrl": "https://open.spotify.com/track/2W7tzqjAPXG97RthXx6pNR"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1986,
+    "artist": "Peter Lundblad",
+    "title": "Ta mig till havet",
+    "spotifyUrl": "https://open.spotify.com/track/01LWBjJwFdRSCeovqipb3D"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1991,
+    "artist": "Angel",
+    "title": "Sommaren i City",
+    "spotifyUrl": "https://open.spotify.com/track/4XoOCFKvikhwp0UcpBwJsO"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1992,
+    "artist": "Lasse Berghagen",
+    "title": "Stockholm i mitt hjärta",
+    "spotifyUrl": "https://open.spotify.com/track/7GW8gHSaruetBuMbRSdma9"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1994,
+    "artist": "Glenmark Eriksson Strömstedt",
+    "title": "När vi gräver guld i USA",
+    "spotifyUrl": "https://open.spotify.com/track/3iAJRvvn8pywXZtvBNNP71"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1995,
+    "artist": "Gyllene Tider",
+    "title": "Kung av sand",
+    "spotifyUrl": "https://open.spotify.com/track/2pIpooJWX5q8DIctvrdQP2"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1996,
+    "artist": "Gyllene Tider",
+    "title": "Juni, Juli, Augusti",
+    "spotifyUrl": "https://open.spotify.com/track/2AXTRWWGTwgp0Ud8cPzT5S"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 1998,
+    "artist": "Markoolio",
+    "title": "Sommar och sol",
+    "spotifyUrl": "https://open.spotify.com/track/4bpqcGVSveDZ5E3rgr9v2y"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2000,
+    "artist": "Håkan Hellström",
+    "title": "Känn ingen sorg för mig Göteborg",
+    "spotifyUrl": "https://open.spotify.com/track/2yz5IaQLnO8WBCIWMXoRBS"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2002,
+    "artist": "kent",
+    "title": "Sverige",
+    "spotifyUrl": "https://open.spotify.com/track/3LodQuubJHwPgkxp4BxHE7"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2004,
+    "artist": "Nic & The Family",
+    "title": "Hej Monica",
+    "spotifyUrl": "https://open.spotify.com/track/52vLuv7YSkhX27eMg4wv2L"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2005,
+    "artist": "Håkan Hellström",
+    "title": "En midsommarnattsdröm",
+    "spotifyUrl": "https://open.spotify.com/track/2PD70CPXPOsnTxJdoaaN95"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2008,
+    "artist": "Veronica Maggio",
+    "title": "Måndagsbarn",
+    "spotifyUrl": "https://open.spotify.com/track/4MXHBiKcdqHBJ4PeHQJBHO"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2011,
+    "artist": "Veronica Maggio",
+    "title": "Jag kommer",
+    "spotifyUrl": "https://open.spotify.com/track/5M2y8QdDepGYMsWyxhoxQU"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2012,
+    "artist": "Mando Diao",
+    "title": "Strövtåg i hembygden",
+    "spotifyUrl": "https://open.spotify.com/track/7KOegFQMd13ETQry4A6Tu1"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2013,
+    "artist": "Avicii",
+    "title": "Wake Me Up",
+    "spotifyUrl": "https://open.spotify.com/track/0nrRP2bk19rLc0orkWPQk2"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2013,
+    "artist": "Håkan Hellström",
+    "title": "Valborg",
+    "spotifyUrl": "https://open.spotify.com/track/0Hz0ySp6XYrBhEQPU16Hz8"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2013,
+    "artist": "Laleh",
+    "title": "Goliat",
+    "spotifyUrl": "https://open.spotify.com/track/7qA8S7mbV9uPy9rFprdN3C"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2015,
+    "artist": "Miriam Bryant",
+    "title": "Ett sista glas",
+    "spotifyUrl": "https://open.spotify.com/track/1SZDQMXX1FrA97YY7Lt3Fj"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2016,
+    "artist": "Samir & Viktor",
+    "title": "Bada nakna",
+    "spotifyUrl": "https://open.spotify.com/track/68jkh9nqSjpmRtkkA9f0ld"
+  },
+  {
+    "category": "Svensk sommar",
+    "year": 2019,
+    "artist": "Mares",
     "title": "Sunnanvind",
     "spotifyUrl": "https://open.spotify.com/track/6FeOQ2oiV6z6xIoFSI9Y1T"
   },
@@ -29,3 +244,4 @@
     "title": "Hälsa Gud",
     "spotifyUrl": "https://open.spotify.com/track/5g9gZGiXlk747pESQzmmRT"
   }
+];
